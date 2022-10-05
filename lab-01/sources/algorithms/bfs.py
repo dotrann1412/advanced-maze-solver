17693d49d4c644250b8cbdea2423d20f24c8700f
dotrann1412 <dotrann.1412@gmail.com>
2022-10-05 20:04:16 +0700

    implement 2 modes of dfs and bfs

--- lab-01/sources/algorithms/bfs.py
+++ lab-01/sources/algorithms/bfs.py
@@ -1,39 +1,136 @@
-from algorithms_utils import direction, valid_graph, AlgorithmsMode
+from algorithms_utils import direction, extra_direction, valid_graph, AlgorithmsMode
 
-def __detect_starting_point(graph):
-	pass
-
-def __normal_bfs(graph):
+def __normal_bfs(graph, callback):
 	dim = [len(graph), len(graph[0])]
 	queue = []
 	starting_point = __detect_starting_point(graph)
-	parrent = [[for __ in range(dim[1])] for _ in (dim[0])]
-	queue.append(starting_point)
+
+	if starting_point[0] < 0 or starting_point[0] >= dim[0] or starting_point[1] < 0 or starting_point[1] >= dim[1]:
+		return None
+
+	parrent = [[None for __ in range(dim[1])] for _ in (dim[0])]
 	
+	queue.append(starting_point)
+	parrent[*starting_point] = starting_point
+	
+	found = False
 
+	while len(queue) != 0 and not found:
+		current = queue[0]
+		queue.pop(0)
+		for element in extra_direction:
+			next_step = current[0] + element[0], current[1] + element[1]
+			
+			if next_step[0] < 0 or next_step[0] >= dim[0] or next_step[1] < 0 or next_step[1] >= dim[1]:
+				continue
 
-def __bfs_with_bonus_point(graph):
+			if isExit(graph[*next_step]):
+				parrent[*next_step] = current
+				found = True
+				break
+
+			if isEmptyCell(graph[*next_step]) and not parrent[*next_step]:
+				parrent[*next_step] = current
+				queue.append(next_step)
+
+	wayout_position = __detect_exit_way(graph)
+
+	if not wayout_position or not parrent[*wayout_position]:
+		return None
+
+	answer = []
+	pointer = wayout_position
+
+	while parrent[*pointer] != starting_point:
+		answer.append(pointer)
+		pointer = parrent[*pointer]
+
+	return answer[::-1]
+
+def __bfs_with_bonus_point(graph, callback):
+	dim = [len(graph), len(graph[0])]
 	starting_point = __detect_starting_point(graph)
 
-def __bfs_intermediate_point(graph):
+def __bfs_intermediate_point(graph, callback):
 	starting_point = __detect_starting_point(graph)
 
-def __bfs_with_teleport_point(graph):
+def __bfs_with_teleport_point(graph, callback):
 	starting_point = __detect_starting_point(graph)
+	queue = []
 
-def bfs(graph, mode):
+	teleport_list = teleport_list(graph)
+	is_used_teleport = False
+
+	if starting_point[0] < 0 or starting_point[0] >= dim[0] or starting_point[1] < 0 or starting_point[1] >= dim[1]:
+		return None
+
+	parrent = [[None for __ in range(dim[1])] for _ in (dim[0])]
+	
+	if isTeleport(graph[*starting_point]):
+		for teleport in teleport_list:
+			parrent[*teleport] = starting_point
+			queue.append(teleport)
+		is_used_teleport = True
+	else:
+		queue.append(starting_point)
+		parrent[*starting_point] = starting_point
+
+	found = False
+
+	while len(queue) != 0 and not found:
+		current = queue[0]
+		queue.pop(0)
+
+		for element in extra_direction:
+			next_step = current[0] + element[0], current[1] + element[1]
+			if next_step[0] < 0 or next_step[0] >= dim[0] or next_step[1] < 0 or next_step[1] >= dim[1]:
+				continue
+
+			if isExit(graph[*next_step]):
+				parrent[*next_step] = current
+				found = True
+				break
+
+			if isTeleport(*next_step) and not is_used_teleport:
+				for teleport in teleport_list:
+					parrent[*teleport] = starting_point if teleport != next_step else current
+					queue.append(teleport)
+				is_used_teleport = True
+				continue
+
+			if isEmptyCell(graph[*next_step]) and not parrent[*next_step]:
+				parrent[*next_step] = current
+				queue.append(next_step)
+
+
+	wayout_position = __detect_exit_way(graph)
+
+	if not wayout_position or not parrent[*wayout_position]:
+		return None
+
+	answer = []
+	pointer = wayout_position
+
+	while parrent[*pointer] != starting_point:
+		answer.append(pointer)
+		pointer = parrent[*pointer]
+
+	return answer[::-1]
+
+
+def bfs(graph, mode, call_back):
 	if not valid_graph(graph):
 		return None
 
 	if mode == AlgorithmsMode.NORMAL:
-		return __normal_bfs(graph)
+		return __normal_bfs(graph, call_back)
 
 	if mode == AlgorithmsMode.BONUS_POINT:
-		return __bfs_with_bonus_point(graph)
+		return __bfs_with_bonus_point(graph, call_back)
 	
 	if mode == AlgorithmsMode.INTERMEDIATE_POINT:
-		return __bfs_intermediate_point(graph)
+		return __bfs_intermediate_point(graph, call_back)
 	
 	if mode == AlgorithmsMode.TELEPORT_POINT:
-		return __bfs_with_teleport_point(graph)
+		return __bfs_with_teleport_point(graph, call_back)
 
--- lab-01/sources/algorithms/dfs.py
+++ lab-01/sources/algorithms/dfs.py
@@ -1,10 +1,106 @@
+from algorithms_utils import direction, extra_direction, valid_graph, AlgorithmsMode
 
-__graph = None
+def __normal_dfs(graph, callback):
+	dim = [len(graph), len(graph[0])]
+	starting_point = __detect_starting_point(graph)
+	if starting_point[0] < 0 or starting_point[0] >= dim[0] or starting_point[1] < 0 or starting_point[1] >= dim[1]:
+		return None
 
-def __dfs_mode_0(parrent_node, current_node):
-	pass
+	mark = [[False for __ in range(dim[1])] for _ in (dim[0])]
+	answer = []
 
-def dfs(graph, starting_point, mode, call_back = None):
-	if len(starting_point) != 2:
-		raise Exception("Invalid starting point")
+	def __process(current_position):
+		if isExit(graph[*current_position]):
+			return True
 
+		mark[*current_position] = True
+
+		for element in extra_direction:
+			next_step = current[0] + element[0], current[1] + element[1]
+			if next_step[0] < 0 or next_step[0] >= dim[0] or next_step[1] < 0 or next_step[1] >= dim[1]:
+				continue
+
+			if not mark[*next_step] and not isEmptyCell(graph[*next_step]):
+				result = __process(next_step)
+				if result:
+					answer.append(next_step)
+					return True
+
+		mark[*current_position] = False
+		return False
+
+	mark[*starting_point] = True
+	found = __process(starting_point)
+
+	if not found:
+		return None
+
+	return answer[::-1]
+
+def __dfs_with_bonus_point(graph, callback):
+	def __process(current_position):
+		pass
+
+def __dfs_intermediate_point(graph, callback):
+	def __process(current_position):
+		pass
+
+def __dfs_with_teleport_point(graph, callback):
+	starting_point = __detect_starting_point(graph)
+	teleport_list = teleport_list(graph)
+	
+	if starting_point[0] < 0 or starting_point[0] >= dim[0] or starting_point[1] < 0 or starting_point[1] >= dim[1]:
+		return None
+
+	mark = [[False for __ in range(dim[1])] for _ in (dim[0])]
+	answer = []
+
+	def __process(current_position):
+		if isExit(graph[*current_position]):
+			return True
+
+		available_next_step = extra_direction
+
+		if isTeleport(graph[*current_position]):
+			available_next_step += teleport_list
+
+		for element in available_next_step:
+			next_step = current[0] + element[0], current[1] + element[1]
+			if next_step[0] < 0 or next_step[0] >= dim[0] or next_step[1] < 0 or next_step[1] >= dim[1]:
+				continue
+
+			if not mark[*next_step] and not isEmptyCell(graph[*next_step]):
+				
+				mark[*current_position] = True
+				result = __process(next_step)
+				mark[*current_position] = False
+
+				if result:
+					answer.append(next_step)
+					return True
+		
+		return False
+
+	mark[*starting_point] = True
+	found = __process(starting_point)
+
+	if not found:
+		return None
+
+	return answer[::-1]
+
+def dfs(graph, mode, call_back):
+	if not valid_graph(graph):
+		return None
+
+	if mode == AlgorithmsMode.NORMAL:
+		return __normal_dfs(graph, call_back)
+
+	if mode == AlgorithmsMode.BONUS_POINT:
+		return __dfs_with_bonus_point(graph, call_back)
+	
+	if mode == AlgorithmsMode.INTERMEDIATE_POINT:
+		return __dfs_intermediate_point(graph, call_back)
+	
+	if mode == AlgorithmsMode.TELEPORT_POINT:
+		return __dfs_with_teleport_point(graph, call_back)
