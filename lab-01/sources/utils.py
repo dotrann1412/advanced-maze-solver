02595065a9da34f2811637e2f2b47dd67223e689
dotrann1412 <dotrann.1412@gmail.com>
2022-10-14 01:42:34 +0700

    update input format

--- lab-01/sources/utils.py
+++ lab-01/sources/utils.py
@@ -3,32 +3,44 @@ from math import sqrt
 from constants import *
 
 def read_file(file_name: str = 'maze.txt'):
-    f = open(file_name, 'r')
-    n_bonus_points = int(next(f)[:-1])
-
+    
     bonus_points = []
     inter_points = []
     teleport_points = []
+    matrix = None
+    
+    try:
+        with open(file_name, 'r') as fp:
+            n_bonus_points = int(fp.readline())
+            for i in range(n_bonus_points):
+                x, y, reward = map(int, fp.readline().split())
+                bonus_points.append((x, y, reward))
 
-    for i in range(n_bonus_points):
-        x, y, reward = map(int, next(f)[:-1].split(' '))
-        bonus_points.append((x, y, reward))
+            n_itermediate_points = int(fp.readline(' '))
+            for i in range(n_itermediate_points):
+                x, y = map(int, fp.readline().split(' '))
+                inter_points.append((x, y))
 
-    text = f.read()
-    matrix = [list(i) for i in text.splitlines()]
-    f.close()
+            n_telepor_points = int(fp.readline(' '))
+            for i in range(n_telepor_points):
+                x, y, xt, yt = map(int, fp.readline().split(' '))
+                teleport_points.append((x, y, xt, yt))
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == MazeObject.START:
-                start = (i, j)
+            matrix = [list(i) for i in fp.read().splitlines()]
 
-            elif matrix[i][j] == MazeObject.EMPTY:
-                if (i == 0) or (i == len(matrix)-1) or (j == 0) or (j == len(matrix[0])-1):
-                    end = (i, j)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == MazeObject.START:
+                    start = (i, j)
 
-            else:
-                pass
+                elif matrix[i][j] == MazeObject.EMPTY:
+                    if (i == 0) or (i == len(matrix)-1) or (j == 0) or (j == len(matrix[0])-1):
+                        end = (i, j)
+
+    except Exception as err:
+        print('[*] Exception raised while parsing maze input file')
+        print(f'\t-----> Here: {err}')
+        return None, None, None, None, None, None
 
     return matrix, start, end, bonus_points, inter_points, teleport_points
 
