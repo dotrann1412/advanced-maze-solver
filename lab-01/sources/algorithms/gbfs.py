9311955f74d9a4e0f04b9fc2ca7ad248b2d5ea5b
dotrann1412 <dotrann.1412@gmail.com>
2022-10-15 17:52:50 +0700

    use built-in priority queue and add tele-mode

--- lab-01/sources/algorithms/gbfs.py
+++ lab-01/sources/algorithms/gbfs.py
@@ -2,50 +2,7 @@ from numpy import empty
 from algorithms.algorithms_utils import *
 from constants import *
 from utils import euclidean_distance, manhattan_distance
-
-class PriorityQueue(object):
-    '''
-    type must be in ['big', 'small']
-    '''
-    def __init__(self, heuristic, goal, type='big'):
-        assert(len(goal) == 2)
-        assert(heuristic(goal, goal) == 0)
-        assert(type in ['big', 'small'])
-        self.queue = []
-        self.heuristic = heuristic
-        self.goal = goal
-        self.type = type
-
-    def __str__(self) -> str:
-        return ' '.join([str(element) for element in self.queue]) if not self.empty() else 'empty'
-
-    def size(self) -> int:
-        return len(self.queue)
-
-    def empty(self):
-        return self.size() <= 0
-
-    def push(self, element):
-        self.queue.append(element)
-
-    def pop(self):
-        try:
-            arg = 0
-            for i in range(1, len(self.queue)):
-                if self.compare()(self.heuristic(self.queue[i], self.goal), self.heuristic(self.queue[arg], self.goal)):
-                    arg = i
-            item = self.queue[arg]
-            del self.queue[arg]
-            return item
-        except:
-            print('Priority queue is empty!')
-            return None
-
-    def compare(self):
-        if self.type == 'big':
-            return lambda a, b: a > b
-        else:  # small
-            return lambda a, b: a < b
+from queue import PriorityQueue
 
 '''
     Args:
@@ -60,43 +17,39 @@ class PriorityQueue(object):
 '''
 def __normalGbfs(graph, start, end, callback, hf):
     terminated = [-1, -1]
-    dim = [len(graph), len(graph[0])]
+    dim = grapthSize(graph)
 
-    priority_queue = PriorityQueue(heuristic=hf, goal=end, type='small')
+    priority_queue = PriorityQueue() # PriorityQueue(heuristic=hf, goal=end, type='small')
 
-    if start[0] < 0 or start[0] >= dim[0] or start[1] < 0 or start[1] >= dim[1]:
+    if not isInGraph(graph, start):
         print("[DEBUG] Invalid starting point...")
         return None
 
     parent = [[None for __ in range(dim[1])] for _ in range(dim[0])]
 
-    priority_queue.push(start)
+    priority_queue.put((hf(start, end), start))
     parent[start[0]][start[1]] = terminated
 
     found = False
-
-    while priority_queue.size() != 0 and not found:
-        current = priority_queue.pop()
-        if current == end:
-            pass
+    while not priority_queue.empty() and not found:
+        hf_value, current_pos = priority_queue.get()
         
         for element in direction:
-            next_step = current[0] + element[0], current[1] + element[1]
+            next_step_x, next_step_y = current_pos[0] + element[0], current_pos[1] + element[1]
 
-            if next_step[0] < 0 or next_step[0] >= dim[0] or next_step[1] < 0 or next_step[1] >= dim[1]:
+            if not isInGraph(graph, (next_step_x, next_step_y)) or graph[next_step_x][next_step_y] == MazeObject.WALL or parent[next_step_x][next_step_y]:
                 continue
         
-            if next_step == end:
-                parent[end[0]][end[1]] = current
+            if (next_step_x, next_step_y)  == end:
+                parent[end[0]][end[1]] = current_pos
                 found = True
                 break
             
-            if (graph[next_step[0]][next_step[1]] == ' ') and not parent[next_step[0]][next_step[1]]:
-                parent[next_step[0]][next_step[1]] = current
-                priority_queue.push(next_step)
+            parent[next_step_x][next_step_y] = current_pos
+            priority_queue.put((hf((next_step_x, next_step_y), end) ,(next_step_x, next_step_y )))
         
-        if current != start:
-            callback(current[1], current[0], Colors.FRONTIER_COLOR, sleep_time=30)
+        if current_pos != start:
+            callback(current_pos[1], current_pos[0], Colors.FRONTIER_COLOR, sleep_time=0)
 
     if not end or not parent[end[0]][end[1]]:
         print("[DEBUG] Unreachable...")
@@ -110,7 +63,7 @@ def __normalGbfs(graph, start, end, callback, hf):
         pointer = parent[pointer[0]][pointer[1]]
 
     for point in answer[1:]:
-        callback(point[1], point[0], Colors.PATH_COLOR, sleep_time=10)
+        callback(point[1], point[0], Colors.PATH_COLOR, sleep_time=0)
 
     return answer[::-1]
 
@@ -121,7 +74,62 @@ def __gbfsIntermediatePoint(graph, start, end, intermediate_points, callback, hf
     pass
 
 def __gbfsWithTeleportPoint(graph, start, end, teleport_points, callback, hf):
-    pass
+    terminated = [-1, -1]
+    size = grapthSize(graph)
+
+    priority_queue = PriorityQueue()
+
+    if not isInGraph(graph, start):
+        print("[DEBUG] Invalid starting point...")
+        return None
+
+    parent = [[None for __ in range(size[1])] for _ in range(size[0])]
+
+    priority_queue.put((hf(start, end), start))
+    parent[start[0]][start[1]] = terminated
+
+    found = False
+    while not priority_queue.empty() and not found:
+        hf_value, current_pos = priority_queue.get()
+        
+        for element in direction:
+            next_step_x, next_step_y = current_pos[0] + element[0], current_pos[1] + element[1]
+
+            if not isInGraph(graph, (next_step_x, next_step_y)) or graph[next_step_x][next_step_y] == MazeObject.WALL or parent[next_step_x][next_step_y]:
+                continue
+        
+            if (next_step_x, next_step_y)  == end:
+                parent[end[0]][end[1]] = current_pos
+                found = True
+                break
+
+            if (next_step_x, next_step_y) in teleport_points:
+                destination_x, destination_y = teleport_points[(next_step_x, next_step_y)]
+                if not parent[destination_x][destination_y]:
+                    parent[destination_x][destination_y] = current_pos
+                    priority_queue.put((hf((destination_x, destination_y), end) ,(destination_x, destination_y )))
+            
+            parent[next_step_x][next_step_y] = current_pos
+            priority_queue.put((hf((next_step_x, next_step_y), end) ,(next_step_x, next_step_y )))
+        
+        if current_pos != start:
+            callback(current_pos[1], current_pos[0], Colors.FRONTIER_COLOR, sleep_time=0)
+
+    if not end or not parent[end[0]][end[1]]:
+        print("[DEBUG] Unreachable...")
+        return None
+    
+    answer = []
+    pointer = end
+
+    while parent[pointer[0]][pointer[1]] != terminated:
+        answer.append(pointer)
+        pointer = parent[pointer[0]][pointer[1]]
+
+    for point in answer[1:]:
+        callback(point[1], point[0], Colors.PATH_COLOR, sleep_time=0)
+
+    return answer[::-1]
 
 def gbfs(graph, start, end, mode, bonus_points, intermediate_points, teleport_points, callback, hf=manhattan_distance):
     if not isValidGraph(graph):
